9cffc892f970fb11a0d37e6fc9ee0d244f152069
Ayush Gupta <gupta9723@gmail.com>
2015-08-25 00:05:53 +0530

    DB
    Adding database structure

---
+++ Neighborhood/src/main/neighbour/bean/User.java
@@ -0,0 +1,141 @@
+package bean;
+
+public class User {
+	private int id;
+	private String userName;
+	private String name;
+	private String password;
+	private String dob;
+	private String email;
+	private String phone;
+	private String workplace;
+	private String college;
+	private String school;
+	private String language;
+	private String currentAddress;
+	private String lat_lng;
+	private String family_Id;
+	private String locality_Id;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getWorkplace() {
+		return workplace;
+	}
+
+	public void setWorkplace(String workplace) {
+		this.workplace = workplace;
+	}
+
+	public String getCollege() {
+		return college;
+	}
+
+	public void setCollege(String college) {
+		this.college = college;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getCurrentAddress() {
+		return currentAddress;
+	}
+
+	public void setCurrentAddress(String currentAddress) {
+		this.currentAddress = currentAddress;
+	}
+
+	public String getLat_lng() {
+		return lat_lng;
+	}
+
+	public void setLat_lng(String lat_lng) {
+		this.lat_lng = lat_lng;
+	}
+
+	public String getFamily_Id() {
+		return family_Id;
+	}
+
+	public void setFamily_Id(String family_Id) {
+		this.family_Id = family_Id;
+	}
+
+	public String getLocality_Id() {
+		return locality_Id;
+	}
+
+	public void setLocality_Id(String locality_Id) {
+		this.locality_Id = locality_Id;
+	}
+
+}
